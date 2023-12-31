612390bf9b7aeb900173357a02f987fa7505ca47
Paulo Hainosz <phainosz.dev@gmail.com>
2023-12-31 18:47:55 -0300

    Changed theme and added lualine bar

--- nvim/after/plugin/colors.lua
+++
@@ -1,5 +0,0 @@
-require("onedark").setup {
-  style = 'warmer',
-  transparente = true
-}
-require('onedark').load()
--- nvim/nvim/lua/user/packer.lua
+++ nvim/nvim/lua/user/packer.lua
@@ -15,6 +15,11 @@ return require('packer').startup(function(use)
   use ("navarasu/onedark.nvim")
 
   use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use {
     'nvim-treesitter/nvim-treesitter',
     run = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
