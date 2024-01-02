a965fc7acc8daf79d775e740618187ae4ad772ac
Paulo Hainosz <phainosz.dev@gmail.com>
2024-01-02 20:41:25 -0300

    Update script for neovim

--- scripts/3-neovim-config.sh
+++ scripts/3-neovim-config.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+sudo rm -rf ~/.local/share/nvim
+sudo rm -rf ~/.local/state/nvim
+sudo rm -rf ~/.config/nvim/
+
 # quickstart for packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
