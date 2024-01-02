221191faae67b2f8efed73eef307d52449034dac
Paulo Hainosz <phainosz.dev@gmail.com>
2024-01-02 17:46:25 -0300

    Changed scripts and nvim treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "java", "javascript", "go", "lua" },
+  ensure_installed = {
+   "java", "javascript", "go", "lua", "html", "json", "markdown", "markdown_inline", "yaml"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
