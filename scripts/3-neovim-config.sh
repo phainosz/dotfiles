d5870835528128780e8d9ede914874c6f6656fb1
Paulo Hainosz <phainosz.dev@gmail.com>
2024-01-05 21:13:51 -0300

    Update scripts and telescope neovim

--- scripts/3-neovim-config.sh
+++ scripts/3-neovim-config.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+APK=
+DISTRO=$(hostnamectl | grep System | awk '{print $3}')
+
+if [ "$DISTRO" == "Fedora" ]; then  
+  APK=dnf
+elif [ "$DISTRO" == "Ubuntu" ]; then
+  APK=apt
+fi
+
 sudo rm -rf ~/.local/share/nvim
 sudo rm -rf ~/.local/state/nvim
 sudo rm -rf ~/.config/nvim/
 
-#fd for better performance in telescope
-sudo dnf install fd-find -y
+#fd to use with telescope
+sudo "$APK" install fd-find
+
+#ripgrep to use telescope
+sudo "$APK" install ripgrep
 
 # quickstart for packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
