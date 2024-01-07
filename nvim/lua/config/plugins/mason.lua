c66b5ecd5bfd01851c9f429898a4f4cf4ff2aff3
Paulo Hainosz <phainosz.dev@gmail.com>
2024-01-06 22:12:56 -0300

    neovim config todo later

--- nvim/lua/config/plugins/mason.lua
+++ nvim/lua/config/plugins/mason.lua
@@ -23,6 +23,41 @@ return {
           package_pending = "➜",
           package_uninstalled = "✗",
         },
+        keymaps = {
+          ---@since 1.0.0
+          -- Keymap to expand a package
+          toggle_package_expand = "<CR>",
+          ---@since 1.0.0
+          -- Keymap to install the package under the current cursor position
+          install_package = "i",
+          ---@since 1.0.0
+          -- Keymap to reinstall/update the package under the current cursor position
+          update_package = "u",
+          ---@since 1.0.0
+          -- Keymap to check for new version for the package under the current cursor position
+          check_package_version = "c",
+          ---@since 1.0.0
+          -- Keymap to update all installed packages
+          update_all_packages = "U",
+          ---@since 1.0.0
+          -- Keymap to check which installed packages are outdated
+          check_outdated_packages = "C",
+          ---@since 1.0.0
+          -- Keymap to uninstall a package
+          uninstall_package = "X",
+          ---@since 1.0.0
+          -- Keymap to cancel a package installation
+          cancel_installation = "<C-c>",
+          ---@since 1.0.0
+          -- Keymap to apply language filter
+          apply_language_filter = "<C-f>",
+          ---@since 1.1.0
+          -- Keymap to toggle viewing package installation log
+          toggle_package_install_log = "<CR>",
+          ---@since 1.8.0
+          -- Keymap to toggle the help view
+          toggle_help = "g?",
+        },
       },
     })
 
--- nvim/lua/config/plugins/servers.lua
+++ nvim/lua/config/plugins/servers.lua
@@ -7,3 +7,14 @@ return {
   "gopls",
   "jdtls"
 }
+--change in future to:
+--lua_ls = {
+--    Lua = {
+--      workspace = { checkThirdParty = false },
+--      telemetry = { enable = false },
+--      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+--      -- diagnostics = { disable = { 'missing-fields' } },
+--    },
+--  },
+--  and use it as vim.tbl_keys(server)
+--  see more at nvim kickstart
