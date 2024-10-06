034387164fdc61cc64eae337c113d28fb257d8ef
Paulo <phainosz.dev@gmail.com>
2024-10-06 18:28:43 -0300

    Update vscode to use neovim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,9 @@
-require("config")
+if vim.g.vscode then
+  -- VSCode extension
+  require("config.core.options")
+  require("config.core.remap")
+  require("config.core.vscode-remap")
+else
+  -- ordinary Neovim
+  require("config")
+end
