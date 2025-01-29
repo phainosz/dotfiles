4d0ff7c2553d217cbf2b4096ef05eb9f65be5200
Paulo <phainosz.dev@gmail.com>
2025-01-29 09:09:41 -0300

    Updated zed, vscode and neovim

--- nvim/lua/config/plugins/gitsigns.lua
+++ nvim/lua/config/plugins/gitsigns.lua
@@ -10,5 +10,32 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+
+    on_attach = function(bufnr)
+      local gitsigns = require('gitsigns')
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end)
+
+      map('n', '[c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end)
+    end,
   }
 }
